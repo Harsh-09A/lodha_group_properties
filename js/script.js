63f5cdc530ad92fc724ec18d73c47a87a516f18c
Harsh Athavale <harsh.autowebbed@gmail.com>
2024-04-01 10:37:31 +0530

    completed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle Content Button
+function toggleContent(dotsId, moreTextId, btnId) {
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreTextId);
+  var btnText = document.getElementById(btnId);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More ...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
